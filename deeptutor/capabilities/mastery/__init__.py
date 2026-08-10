b85966b65d26ffab89863dbe142ff017156dc7f9
kitty4-disign <2811800455@qq.com>
2026-08-10 21:59:23 +0800

    fix(k12): install server-side mastery quiz guard

--- deeptutor/capabilities/mastery/__init__.py
+++ deeptutor/capabilities/mastery/__init__.py
@@ -1,6 +1,9 @@
 """Mastery path loop capability."""
 
 from deeptutor.capabilities.mastery.loop import MasteryLoopCapability
+from deeptutor.capabilities.mastery.quiz_guard import install_mastery_quiz_guard
 from deeptutor.capabilities.mastery.tools import MASTERY_TOOL_NAMES, MASTERY_TOOL_TYPES
+
+install_mastery_quiz_guard()
 
 __all__ = ["MASTERY_TOOL_NAMES", "MASTERY_TOOL_TYPES", "MasteryLoopCapability"]
